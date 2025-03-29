e478cb94cb60dcb8c88d7060b834707d81975c1e
mtalala <superraioi2014@gmail.com>
2025-03-29 17:38:02 -0300

    atualização

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,17 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <h1 className="titulo">Folha da Inovação</h1>
-        <header>
-          <nav>
-              <ol>
-                  <li>
-                      <Link href="/">| Home |</Link>
-                  </li>
-              </ol>
-          </nav>
-        </header>
-        <hr></hr>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,77 @@
 import styles from "./page.module.css";
-import Link from 'next/link';
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className={styles.alignall}>
-      <div className={styles.align}>
-        <Link href="/aida">
-          <div className={styles.noticia}>
-            <div className={styles.tag}><time>21.10.2024</time> - Notícia</div>
-            <h1>Conheça Ai-Da</h1>
-            <div className={styles.preview}>
-              <p>A robô humanoide Ai-Da, que já foi detida no Egito sob suspeita de espionagem e fez história ao expor na Bienal de Veneza, será a primeira de sua “espécie” a ter uma obra vendida por uma casa de leilões renomada.</p>
-            </div>
-            <div className={styles.tags}>
-              <h1>Veneza</h1>
-              <h1>Bienal</h1>
-              <h1>Arte</h1>
-              <h1>Robô</h1>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <div className={styles.container}>
 
-      <div className={styles.align}>
-        <Link href="/foguete">
-          <div className={styles.noticia}>
-            <div className={styles.tag}><time>14.10.2024</time> - Notícia</div>
-            <h1>Foguete dando ré?</h1>
-            <div className={styles.preview}>
-              <p>A SpaceX, empresa do bilionário Elon Musk, alcançou um feito impressionante ao lançar e capturar de volta o primeiro estágio do foguete Starship.</p>
-            </div>
-            <div className={styles.tags}>
-              <h1>SpaceX</h1>
-              <h1>Nave</h1>
-              <h1>Starship</h1>
-              <h1>Astronomia</h1>
-            </div>
-          </div>
-        </Link>
-      </div>
+        <div className={styles.card}>
 
-      <div className={styles.align}>
-        <Link href="/alexa">
-          <div className={styles.noticia}>
-            <div className={styles.tag}><time>19.09.2024</time> - Notícia</div>
-            <h1>Paciente usa a mente para controlar Alexa</h1>
-            <div className={styles.preview}>
-              <p>Um homem de 64 anos com esclerose lateral amiotrófica (ELA) conseguiu controlar a assistente virtual Alexa usando o pensamento após passar por um implante cerebral de chip nos Estados Unidos.</p>
-            </div>
-            <div className={styles.tags}>
-              <h1>Alexa</h1>
-              <h1>Neuralink</h1>
-              <h1>Cérebro</h1>
-              <h1>Paciente</h1>
-            </div>
+          <div className={styles.data}>
+            <time>21.10.2024</time> 
+            - Notícia
           </div>
-        </Link>
-      </div>
 
+          <h1 className={styles.title}>Title</h1>
+
+          <div className={styles.preview}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit orci, laoreet eget sapien nec, ullamcorper molestie ante. Etiam quis mollis lectus. Nullam pulvinar dolor magna, ut hendrerit nisl facilisis vel. Sed lacinia orci vel nunc sodales accumsan. Fusce dapibus augue id magna congue, in mattis velit facilisis.</p>
+          </div>
+
+          <div className={styles.tags}>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+          </div>
+
+        </div>
+      
+        <div className={styles.card}>
+
+          <div className={styles.data}>
+            <time>21.10.2024</time> 
+            - Notícia
+          </div>
+
+          <h1 className={styles.title}>Title</h1>
+
+          <div className={styles.preview}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit orci, laoreet eget sapien nec, ullamcorper molestie ante. Etiam quis mollis lectus. Nullam pulvinar dolor magna, ut hendrerit nisl facilisis vel. Sed lacinia orci vel nunc sodales accumsan. Fusce dapibus augue id magna congue, in mattis velit facilisis.</p>
+          </div>
+
+          <div className={styles.tags}>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+          </div>
+          
+        </div>
+
+        <div className={styles.card}>
+
+          <div className={styles.data}>
+            <time>21.10.2024</time> 
+            - Notícia
+          </div>
+
+          <h1 className={styles.title}>Title</h1>
+
+          <div className={styles.preview}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit orci, laoreet eget sapien nec, ullamcorper molestie ante. Etiam quis mollis lectus. Nullam pulvinar dolor magna, ut hendrerit nisl facilisis vel. Sed lacinia orci vel nunc sodales accumsan. Fusce dapibus augue id magna congue, in mattis velit facilisis.</p>
+          </div>
+
+          <div className={styles.tags}>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+            <p className={styles.tag}>Tag</p>
+          </div>
+          
+        </div>
+
+      
     </div>
   );
 }
